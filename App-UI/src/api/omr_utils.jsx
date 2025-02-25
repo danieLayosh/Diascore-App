f3b64df084a378a4855564054a86d2de4f77ded3
danieLayosh <danielayosh@gmail.com>
2025-02-25 20:05:04 +0200

    Add omrRequest function for OMR API file upload

--- App-UI/src/api/omr_utils.jsx
+++ App-UI/src/api/omr_utils.jsx
@@ -1,1 +1,21 @@
+
 // Usefull function to communicate with the OMR API
+export const omrRequest = async (file1, file2, pORt, kORs) => {
+    const apiKey = import.meta.env.VITE_OMR_API_KEY; 
+    const url = import.meta.env.VITE_OMR_API_URL;
+
+    const formData = new FormData();
+    formData.append("files", file1);
+    formData.append("files", file2); 
+    formData.append("pORt", pORt);
+    formData.append("kORs", kORs);
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        'x-api-key': apiKey,
+      },
+      body: formData,
+    });
+    return response.json();
+};
