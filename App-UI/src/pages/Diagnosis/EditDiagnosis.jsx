2ce78a0b8dedc47836a45cce2545d8e86e07cf4b
danieLayosh <danielayosh@gmail.com>
2025-03-03 11:26:44 +0200

    Add ConfirmDontSave modal and update Diascore header to trigger modal

--- App-UI/src/pages/Diagnosis/EditDiagnosis.jsx
+++ App-UI/src/pages/Diagnosis/EditDiagnosis.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import { useNavigate } from "react-router-dom";
@@ -9,12 +9,16 @@ import Loader from "../../components/Loader";
 import ActionNewDiagnosis from "../../components/dropdown/ActionNewDiagnosis";
 import EditDiagnosisForm from "../../components/forms/EditDiagnosisForm";
 import { useParams } from 'react-router-dom';
+import { ConfirmDontSave } from "../../components/modal/ConfirmDontSave";
 
 const EditDiagnosis = () => {
+    // eslint-disable-next-line no-unused-vars
     const { user, loading } = useAuth();
     const navigate = useNavigate();
     const { showAlert } = useAlert();
     const { id } = useParams(); // Get the id from the URL
+    const [confirmReturnOpen, setConfirmReturnOpen] = useState(false);
+
 
     const handleSignOut = async () => {
         try {
@@ -27,6 +31,11 @@ const EditDiagnosis = () => {
         }
     };
 
+    const handleDiascoreClick = () => {
+        setConfirmReturnOpen(true); // Open the modal
+    };
+    
+
     if (loading) {
         return <div className="flex h-screen items-center justify-center size-max w-screen bg-slate-900"> <Loader/> </div>;
     }
@@ -35,7 +44,7 @@ const EditDiagnosis = () => {
         <div className="flex flex-col items-start bg-gradient-bg text-text-light min-h-screen">
             {/* Header Section */}
             <div className="flex justify-between items-center w-full px-4 sm:px-12 lg:px-12 py-4 bg-card-bg shadow-lg">
-                <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
+                <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold" onClick={handleDiascoreClick}>Diascore</h3>
                 <div className="flex gap-6">
                     <ActionNewDiagnosis />
                     <ProfileButton 
@@ -49,6 +58,11 @@ const EditDiagnosis = () => {
             <div className="mx-auto justify-center items-center lg:mt-24 sm:mt-8 mt-24"> 
                 <EditDiagnosisForm id={id} />
             </div>
+            <ConfirmDontSave 
+                isOpen={confirmReturnOpen} 
+                onOpenChange={setConfirmReturnOpen} 
+            />
+
         </div>
     );
 };
