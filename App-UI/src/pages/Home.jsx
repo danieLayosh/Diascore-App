8f9058dd08209dd5532bf1c85f7115dc47de2cfd
danieLayosh <danielayosh@gmail.com>
2025-03-03 11:26:40 +0200

    Add navigation to welcome page on Diascore header click

--- App-UI/src/pages/Home.jsx
+++ App-UI/src/pages/Home.jsx
@@ -79,7 +79,7 @@ const Home = () => {
         <div className="flex flex-col items-start w-screen bg-gradient-bg text-text-light min-h-screen">
             {/* Header Section */}
             <div className="flex justify-between items-center w-full px-4 sm:px-12 lg:px-12 py-4 bg-card-bg shadow-lg">
-                <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
+                <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold" onClick={() => {navigate('/welcome')}}>Diascore</h3>
                 <div className="flex gap-6">
                     <Action />
                     <ProfileButton 
