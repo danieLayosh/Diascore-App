e1636ebfd068a3050172d7905eaab366cdb8435d
danieLayosh <danielayosh@gmail.com>
2025-03-05 22:19:12 +0200

    Enhance DiagnosisTest component layout and integrate DiagnosisTestForm

--- App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
+++ App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
+import GreenCoverButton from "../../components/buttons/GreenCoverButton";
+import { DiagnosisTestForm } from "../../components/link/DiagnosisTestForm";
 
 export const DiagnosisTest = () => {
     
     // get the linkId from the URL
     const { linkId } = useParams();
 
-
     return (
-        <div>
-            <h1>Diagnosis Test for {linkId}</h1>
+        <div className="flex flex-col items-start bg-gradient-bg text-text-light min-h-screen">
+            <div className="flex justify-between items-center w-full px-4 sm:px-12 lg:px-12 py-4 bg-card-bg shadow-lg">
+                <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
+                <h3 className="text-5xl sm:text-6xl  text-white ">Diagnostic form</h3>
+                <GreenCoverButton text="Submit" defaultColor="black" onClick={() => {console.log("GreenCoverButton clicked")}} />
+            </div>
+            <div className="flex flex-col items-center w-full px-4 sm:px-12 lg:px-12 py-4">
+                <DiagnosisTestForm linkId={linkId} />
+            </div>
         </div>
     );
 };
