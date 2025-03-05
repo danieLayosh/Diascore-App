4f70ad154a6f786a668a8c0c540223baf96f83d5
danieLayosh <danielayosh@gmail.com>
2025-03-05 21:06:07 +0200

    Display Diagnosis Test title with linkId from URL

--- App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
+++ App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
@@ -1,10 +1,14 @@
-
+import { useParams } from "react-router-dom";
 
 export const DiagnosisTest = () => {
     
+    // get the linkId from the URL
+    const { linkId } = useParams();
+
+
     return (
         <div>
-            <h1>Diagnosis Test</h1>
+            <h1>Diagnosis Test for {linkId}</h1>
         </div>
     );
 };
