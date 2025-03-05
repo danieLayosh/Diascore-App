8e33ddd4952cac4b7155e293193bc885cce61202
danieLayosh <danielayosh@gmail.com>
2025-03-05 23:58:44 +0200

    Refactor DiagnosisTest component layout and comment out unused GreenCoverButton

--- App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
+++ App-UI/src/pages/Diagnosis/DiagnosisTest.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import GreenCoverButton from "../../components/buttons/GreenCoverButton";
+// import GreenCoverButton from "../../components/buttons/GreenCoverButton";
 import { DiagnosisTestForm } from "../../components/link/DiagnosisTestForm";
 
 export const DiagnosisTest = () => {
@@ -11,10 +11,13 @@ export const DiagnosisTest = () => {
         <div className="flex flex-col items-start bg-gradient-bg text-text-light min-h-screen">
             <div className="flex justify-between items-center w-full px-4 sm:px-12 lg:px-12 py-4 bg-card-bg shadow-lg">
                 <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
-                <h3 className="text-5xl sm:text-6xl  text-white ">Diagnostic form</h3>
-                <GreenCoverButton text="Submit" defaultColor="black" onClick={() => {console.log("GreenCoverButton clicked")}} />
+                <h3 className="text-xl sm:text-6xl lg:text-xl text-white ">Diagnostic form</h3>
+                {/* <GreenCoverButton text="Submit" defaultColor="black" onClick={() => {console.log("GreenCoverButton clicked")}} /> */}
             </div>
-            <div className="flex flex-col items-center w-full px-4 sm:px-12 lg:px-12 py-4">
+            {/* <div className="flex flex-col items-center w-full px-4 sm:px-12 lg:px-12 py-4">
+                <h3 className="text-xl items-center sm:text-6xl lg:text-6xl text-white ">Diagnostic form</h3>
+            </div> */}
+            <div className="flex flex-col items-center w-full px-4 sm:px-12 lg:px-12 py-40">
                 <DiagnosisTestForm linkId={linkId} />
             </div>
         </div>
