d0c3ce974c77da57ca35099e2dc148a98713c337
danieLayosh <danielayosh@gmail.com>
2025-03-05 22:34:02 +0200

    Add getDiagnosisQuestions function to fetch diagnosis questions based on parameters

--- App-UI/src/api/main_requests.jsx
+++ App-UI/src/api/main_requests.jsx
@@ -1,6 +1,7 @@
 export const mainRequest = async (gender, age, pORt, kORs, answers) => {
   const apiKey = import.meta.env.VITE_MAIN_LOGIC_KEY;
-  const url = import.meta.env.VITE_MAIN_API_URL;
+  let url = import.meta.env.VITE_MAIN_API_URL;
+  url = `${url}questions/sum`;
 
   // Convert answers object to a list (array)
   const answersArray = Object.values(answers);
@@ -36,5 +37,37 @@ export const mainRequest = async (gender, age, pORt, kORs, answers) => {
   } catch (error) {
     console.error("Error in mainRequest:", error);
     throw error; // Rethrow error to be handled by the caller
+  }
+};
+
+export const getDiagnosisQuestions = async (kORs, pORt) => {
+  const apiKey = import.meta.env.VITE_MAIN_LOGIC_KEY;
+  let url = import.meta.env.VITE_MAIN_API_URL;
+  if (kORs === "kids" || kORs === "school" && pORt === "p" || pORt === "t") {
+    url = `${url}questions/test/?pORt=${pORt}&kORs=${kORs}`;
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json', 
+          'x-api-key': apiKey,
+        },
+      });
+  
+      // Check if response is OK (status 2xx)
+      if (!response.ok) {
+        throw new Error(`API request failed with status: ${response.status}`);
+      }
+  
+      // Parse the JSON response
+      const responseJson = await response.json();
+      return responseJson;
+  
+    } catch (error) {
+      console.error("Error in mainRequest:", error);
+      throw error; // Rethrow error to be handled by the caller
+    }
+
   }
 };
