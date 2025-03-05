ee9268051efce203e95c626fd678682e6f9e7de2
danieLayosh <danielayosh@gmail.com>
2025-03-05 22:18:53 +0200

    Add endpoint to retrieve questions list for test based on parameters

--- MainLogic-API/app/routers/questions.py
+++ MainLogic-API/app/routers/questions.py
@@ -4,6 +4,7 @@ from services.check_questions.kids import check_questions as check_kids
 from services.check_questions.school import check_questions as check_school
 from services.excel_service import process_all_scores
 from services.validate_request import validate_request_params
+import services.questions_list as questions_list
 
 router_questions = APIRouter()
 
@@ -23,3 +24,9 @@ async def calculate_answers(request: AnswerSumRequest) -> processrequestDone:
 @router_questions.get("/questions/body/")
 async def get_request_body():
     return AnswerSumRequest.schema_json().split()
+
+
+@router_questions.get("/questions/test/")
+async def get_questions_test(pORt: str, kORs: str) -> list[str]:
+    return questions_list.get_questions_list(pORt=pORt, kORs=kORs)
+        
